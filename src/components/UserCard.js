75448f8497892c3fb95d6a23ee8d87183c930361
oljuanfe <ojuanferrero@gmail.com>
2018-11-17 12:52:28 +0100

    Create jsx for user card when new info recieved and display it

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -39,10 +39,49 @@ class UserCard extends Component {
     const { 
       selectChosenUser
     } = this.props;
+    const { currentUser } = this.state;
+    const {
+      avatar_url,
+      login,
+      name,
+      location,
+      public_repos,
+      followers,
+      following,
+      created_at,
+    } = currentUser;
     console.log(selectChosenUser);
+    console.log(currentUser.location, !currentUser.location)
     return (
       <div className="">
-        
+        <div className="user-image">
+          <img src={avatar_url} alt=""/>
+        </div>
+        <div className="user-main-info">
+          <span className="user-login">
+            @{login}
+          </span>
+          <h1 className="user-name">
+            {name}
+          </h1>
+          <div className="user-location">
+            {location}
+          </div>
+        </div>
+        <div className="user-secondary-info">
+          <div className="user-repos">
+            {public_repos} Repos
+          </div>
+          <div className="user-followers">
+            {followers} Followers
+          </div>
+          <div className="user-following">
+            {following} Following
+          </div>
+        </div>
+        <span className="time-as-user">
+          {created_at}
+        </span>
       </div>
     );
   }
